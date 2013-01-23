8dca2d1a1339b43eaaead26f3f081854f4b684fa
Pali Ondras <pali@silvermax.sk>
2013-01-23 11:24:20 +0100

    commiting missing dataobject holding carousel items

---
+++ code/MaxCarouselItem.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* DataObject holding Slide data. If you fill in External link, InternalLink setup will be ignored.
+* You can setup per slide animation effect.
+* @package maxcarousel - silverstripe module for slides management and presentation
+* @link maxskitter https://github.com/Silvermax/maxskitter/
+* @author Pali Ondras
+*/
+
+class MaxCarouselItem extends DataObject
+{
+	static $db = array (
+		"Title" => 'Varchar(1024)', // eg. "Energy Saving Report for Year 2011, New Zealand LandCorp"
+		"Description" => 'Text',
+		'HTMLDescription' => 'HTMLText'	
+	);
+
+	static $has_one = array (
+		'MaxCarouselImage' => 'Image',
+		"InternalLink" => "SiteTree"
+	);
+
+	static $belongs_many_many = array('Page'=>'Page');
+
+	public function getCMSFields()
+	{
+		$fields = new FieldList();
+		
+		$fields->push(new TextField('Title', _t("MaxCarousel.Title","Title")));
+		$fields->push(new TextareaField('Description', _t("MaxCarousel.Description","Description")));
+		
+		$fields->push(new HtmlEditorField("HTMLDescription", _t("MaxCarousel.HTMLDescription","HTMLDescription")));
+	
+		$fields->push(new TextField('ExternalLink', _t("MaxCarousel.ExternalLink","External Link")));
+		
+		$internalLink = new TreeDropdownField("InternalLinkID", _t("MaxCarousel.InternalLink","Internal Link"), "SiteTree");
+		$fields->push($internalLink);
+		
+		if ($this->InternalLinkID > 0) {
+			$fields->push(new CheckboxField("forceToEmpty","Remove internal LinkTo", false) );
+		}
+	
+		$fields->push(new UploadField('MaxCarouselImage'));
+		
+		$this->extend('updateCMSFields', $fields);
+		
+		return $fields;
+	}
+
+// Tell the datagrid what fields to show in the table
+   public static $summary_fields = array(
+		'Title' => 'Title',
+		'Description'=>'Description',
+		'InternalLink.MenuTitle' => 'Internal LinkTo page',
+		'ExternalLink' => 'ExternalLink',
+		'Thumbnail' => 'Image'
+   );
+  
+  // this function creates the thumnail for the summary fields to use
+   public function getThumbnail() {
+     return $this->MaxCarouselImage()->CMSThumbnail();
+  }
+
+	function onBeforeWrite() {
+		parent::onBeforeWrite();
+		
+		// Prefix the URL with "http://" if no prefix is found
+		if($this->ExternalLink && (strpos($this->ExternalLink, '://') === false)) {
+		$this->ExternalLink = 'http://' . $this->ExternalLink;
+		}
+		if (isset($_POST["forceToEmpty"]) && $_POST["forceToEmpty"]) {
+		$this->InternalLinkID = 0;
+		}
+	}
+
+	function Link() {
+		// TODO -> InternaLink + anchor Title
+		if ($this->ExternalLink) return $this->ExternalLink;
+		if ($this->InternalLinkID) return $this->InternalLink()->Link();
+		return false;
+	}
+
+}

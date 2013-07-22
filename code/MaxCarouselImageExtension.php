ba2c976a5d7fc23b10db5debd7a9fe392c9067ba
Pali Ondras <pali.ondras@gmail.com>
2013-07-22 10:26:32 +0200

    Update MaxCarouselImageExtension.php
    use GDBackend instead of GD class

--- code/MaxCarouselImageExtension.php
+++ code/MaxCarouselImageExtension.php
@@ -5,7 +5,7 @@ class MaxCarouselImageExtension extends DataExtension {
 	public static $CarouselImageWidth = 620;
 	public static $CarouselImageHeight = 220;
 	
-	function generateCarouselImageSize(GD $gd) {
+	function generateCarouselImageSize(GDBackend $gd) {
 		return $gd->croppedResize(self::$CarouselImageWidth,self::$CarouselImageHeight);
 	}
 	
